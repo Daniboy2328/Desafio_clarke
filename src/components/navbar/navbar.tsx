9cbe0d41ae1132e6e9b53d3a0dd6af96b489b931
Daniboy2328 <danielalvesandrade2003@gmail.com>
2024-01-17 20:25:14 -0300

    Form concluido, ajustes finais na responsividade.

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,13 +1,13 @@
 import './navbar.css';
 export function Navbar (){
  return(
-    <nav>
+    <div className='navbar'>
         <img src="src\images\clarke_energia_BRANCO-1.png" alt="Logo" className='logo'/>
             <div className='links'>
-                <h1>Sobre nós</h1>
-                <h1>Contato</h1>
-                <button>Seja nosso cliente</button>
+                <h1 className='info'>Sobre nós</h1>
+                <h1 className='info'>Contato</h1>
+                <button className='botao'>Seja nosso cliente</button>
             </div>
-    </nav>
+    </div>
  );
 }
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,15 +1,28 @@
 import './index.css'
 export function Index (){
     return (
-        <main>
+        <main className='corpo'>
             <div className='leftframe'>
-                <h2>
+                <h2 className='tituloEsquerda'>
                 Não jogue dinheiro fora!<br/>Encontre o melhor<br/>fornecedor para as<br/>suas necessidades.
                 </h2>
                 <img src="src\images\Wind turbine-bro.png" className='eolica'/>
             </div>
             <div className='rightframe'>
                 <div className='inputBox'>
+                    <h2 className='titulo'>
+                        Preencha o campo para achar<br/>o melhor fornecedor.
+                    </h2>
+
+                    <h2 className='subtitulo'>
+                        Qual é o consumo mensal de energia<br/>da sua empresa?
+                    </h2>
+
+                    <input className='entrada' placeholder='Digite aqui.'>
+                    </input>
+                    <button className='calcular'>
+                        Calcular
+                    </button>
                 </div>
             </div>
         </main>
